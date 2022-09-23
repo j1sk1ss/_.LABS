3d054790becea39dc4dff40a1282add95c5229ff
j1sk1ss <fotikolay222@gmail.com>
2022-09-23 15:49:44 +0500

    Initial commit

---
+++ CS_LABS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_LABS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
