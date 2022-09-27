a56f4373b9078cc800252c6cb76627939bf42388
j1sk1ss <fotikolay222@gmail.com>
2022-09-27 21:40:18 +0500

    opt 9.27.2022

--- CS_LABS/LABS/Quest_4.cs
+++ CS_LABS/LABS/Quest_4.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using CS_LABS.SUP_CLASSES;
-using Math = CS_LABS.SUP_CLASSES.Math;
-
 namespace CS_LABS.LABS;
 
 public class Quest4
