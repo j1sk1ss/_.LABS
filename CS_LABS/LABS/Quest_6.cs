3e4ee4121974cc808d82c8debc9743224d263479
wlad78787 <tghhspirt@gmail.com>
2022-09-27 15:40:30 +0300

    test bibs push

--- CS_LABS/LABS/Quest_6.cs
+++ CS_LABS/LABS/Quest_6.cs
@@ -29,17 +29,9 @@ public class Quest6
             sw.Write(tmp2);
         }
     }
-
+        // Test
     private void Work_2()
     {
-        Console.WriteLine("This work where part of text from file will deleted:"); 
 
-        var tmp =File.ReadAllText(@"C:\Users\tghhs\RiderProjects\CS_LABS\CS_LABS\Test.txt", Encoding.UTF8); 
-        Console.WriteLine(tmp); 
-        var tmp2 = tmp.Substring(0, tmp.Length / 2); 
-        using (var sw = new StreamWriter(@"C:\Users\tghhs\RiderProjects\CS_LABS\CS_LABS\Test.txt")) 
-        { 
-            sw.Write(tmp2); 
-        }
     }
 }
