f317cf58f0e9fffe75b25d2977d255055e1f9309
wlad78787 <tghhspirt@gmail.com>
2022-09-27 17:24:16 +0300

    6l.c->1,2.uw->3

--- CS_LABS/LABS/Quest_6.cs
+++ CS_LABS/LABS/Quest_6.cs
@@ -10,28 +10,51 @@ public class Quest6
     private readonly Labs _labs = new Labs();
     public void Main()
     {
-        var voids = new Action[2];
+        var voids = new Action[3];
         voids[0] = Work_1;
+        voids[1] = Work_2;
+        voids[2] = Work_3;
         _labs.AddVoids(voids);
         Console.WriteLine("Choose a work number: ");
         _labs.Works[_labs.Math.ToInt(Console.ReadLine())]();
     }
 
+    private const string Path = @"C:\Users\tghhs\RiderProjects\CS_LABS\CS_LABS\";
     private static void Work_1()
     {
         Console.WriteLine("This work where part of text from file will deleted:");
-        var path = @"C:\Users\tghhs\RiderProjects\CS_LABS\CS_LABS\";
-        var tmp =File.ReadAllText($"{path}Test.txt", Encoding.UTF8);
+        var tmp = File.ReadAllText($"{Path}Test.txt", Encoding.UTF8);
         Console.WriteLine(tmp);
-        var tmp2 = tmp.Substring(0, tmp.Length / 2);
-        using (var sw = new StreamWriter($"{path}Test.txt"))
-        {
-            sw.Write(tmp2);
-        }
+        using var sw = new StreamWriter($"{Path}Test.txt");
+        sw.Write(tmp[..(tmp.Length / 2)]);
     }
-        // Test
-    private void Work_2()
+    private static void Work_2()
     {
-        
+        Console.WriteLine("This work where program should separate dates and month to to different files: ");
+        var days = "";
+        var month = "";
+             var tmp = (File.ReadAllText($"{Path}Dates.txt", Encoding.UTF8)).Split(" ");
+             foreach (var t in tmp)
+             {
+                 days += t[0].ToString();
+                 days += t[1].ToString() + " ";
+
+                 month += t[3].ToString();
+                 month += t[4].ToString() + " ";
+             }
+             using (var sw = new StreamWriter($"{Path}Days.txt"))
+             {
+                 sw.WriteAsync(days);
+             }
+             using (var sw = new StreamWriter($"{Path}Months.txt"))
+             {
+                 sw.WriteAsync(month);
+             }
+    }
+
+    private void Work_3()
+    {
+        var matrixA = File.ReadAllText($"{Path}MatrixA.txt").Split("\n");
+        var matrixB = File.ReadAllText($"{Path}MatrixB.txt").Split("\n");
     }
 }
